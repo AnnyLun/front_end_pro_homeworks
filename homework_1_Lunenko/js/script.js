931b782beaa60d2c61cc2f206ffe7ae83e191750
Anna Lunenko <rozzaly1994@gmail.com>
2022-05-30 19:54:20 +0300

    first commit

---
+++ homework_1_Lunenko/js/script.js
@@ -0,0 +1,10 @@
+firstNumber = 10;
+secondNumber = Math.PI; 
+thirdNumber = Math.pow(20,2);
+mult = firstNumber * secondNumber * thirdNumber / 4
+console.log("**************")
+console.log("Обьем цилиндра с Висотой цилиндра h =", 10, "Математической постоянной П =", 3.1415926535, "и Диаметром основания цилиндра d=", 20, "равен:");
+console.log("--------------------");
+console.log("V = ", mult);
+console.log("--------------------");
+console.log("end.");
