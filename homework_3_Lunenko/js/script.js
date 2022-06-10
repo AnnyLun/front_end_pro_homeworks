41ebc2bc54d40b82a2179b34c9a8028983eaada2
Anna Lunenko <rozzaly1994@gmail.com>
2022-06-11 02:56:47 +0300

    added prceInt and replaceAll

--- homework_3_Lunenko/js/script.js
+++ homework_3_Lunenko/js/script.js
@@ -1,13 +1,13 @@
 debugger;
-a = +prompt("Сколько будет 2+2?");
+a = parseInt(prompt("Сколько будет 2+2?").replaceAll(" ", ""));
 firstNumb = 0;
-if (a === 4) {
+if (a == 4) {
     a = firstNumb + 10; 
 } else {
     a = firstNumb - 0; 
 }
 console.log(a);
-b = prompt("Солнце встает на востоке?");
+b = prompt("Солнце встает на востоке?").replaceAll(" ", "");
 if (b === "Да") {
     a = a + 10;
 } 
@@ -15,22 +15,22 @@ else {
     a = a - 0;
 }
 console.log(a);
-с = prompt("Сколько будет 5 / 0?");
-if (с === "Нельзя" || c === "Делить на 0 нельзя" || c === "Ничего") {
+с = prompt("Сколько будет 5 / 0?").replaceAll(" ", "");
+if (с === "Нельзя" || c === "Ничего") {
     a = a + 10;
 } else {
     a = a - 0;
 }
 console.log(a);
-d = prompt("Какого цвета небо?");
+d = prompt("Какого цвета небо?").replaceAll(" ", "");
 if (d === "Голубой") {
     a = a + 10;
 } else {
     a = a - 0;
 }
 console.log(a);
-f = +prompt("Какой правильный ответ на главный вопрос жизни, вселенной и всего такого?");
-if (f === 42) {
+f = parseInt(prompt("Какой правильный ответ на главный вопрос жизни, вселенной и всего такого?").replaceAll("", " "));
+if (f == 42) {
     a = a + 10;
 } else {
     a = a - 0;
