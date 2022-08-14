e24c8b533309e699762b3dcd9e87e8b0d439d866
Anna Lunenko <rozzaly1994@gmail.com>
2022-08-14 19:07:45 +0300

    corrections of HW_22

--- homework_22_Lunenko/js/script.js
+++ homework_22_Lunenko/js/script.js
@@ -1,28 +1,65 @@
-const xhr = new XMLHttpRequest()
-xhr.open("GET", "data.json", false)
-xhr.send()
-
-const xhrTwo = new XMLHttpRequest()
-xhrTwo.open("GET", "data2.json", false)
-xhrTwo.send()
-
-let responseDataFirst = xhr.response;
-let responseDataSecond = xhrTwo.response;
+// Variant #1
 let parseData = data => JSON.parse(data)
-let parseDataFirst = parseData(responseDataFirst)
-let parseDataSecond = parseData(responseDataSecond)
-
 let newArr = [];
-function returnObject (object) {
-    for (let key in object) {
-        pushArrData(object[key]);
-    }
-}
-returnObject (parseDataFirst);
-returnObject (parseDataSecond);
 
 function pushArrData (arr) {
-    arr.forEach((item) => 
-    newArr.push(item))
+    arr.forEach(item => newArr.push(item))
+    if (newArr.length > 3) 
+        console.log(newArr)
 }
-console.log(newArr);
+function getResponse (data, callback) {
+    const xml = new XMLHttpRequest()
+    xml.open("GET", data)
+    xml.send()
+    xml.addEventListener("readystatechange", () => {
+        if(xml.readyState === 4 && xml.status > 199 && xml.status < 400) {
+            let response = xml.response;
+            callback (parseData(response).children)
+        }
+    })
+}
+getResponse ("data.json", pushArrData);
+getResponse ("data2.json", pushArrData)
+
+// Variant #2
+// function getResponse(action) {
+//     return new Promise (
+//         function (resolve, reject) {
+//             const xml = new XMLHttpRequest()
+//             xml.open("GET", action)
+//             xml.send()
+//             xml.addEventListener("readystatechange", () => {
+//                 if (xml.readyState === 4) {
+//                     xml.status > 199 && xml.status < 400 
+//                     ? resolve (xml.response)
+//                     : reject (xml.status)
+//                 }
+//             })
+//         }
+//     )
+// }
+// function myresponse (response) {
+//     response = JSON.parse(response).children;
+//     response.forEach(item => newArr.push(item))
+// }
+
+// getResponse ("data.json")
+// .then (
+//     function (action) {
+//         myresponse(action)
+//     }
+// ) 
+// getResponse ("data2.json")
+// .then (
+//     function (action) {
+//         myresponse(action)
+//     }
+// ) 
+// .finally(
+//     function() {
+//         console.log(newArr)
+//     }
+// )
+
+
+
